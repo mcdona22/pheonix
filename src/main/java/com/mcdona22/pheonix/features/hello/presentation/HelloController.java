b10943d20e3e9a6ef923de5f27852e3eee7922c6
John McDonald <mcdona22@gmail.com>
2025-11-25 11:13:08 +0000

    corrected the controller and altered the ci file

--- src/main/java/com/mcdona22/pheonix/features/hello/presentation/HelloController.java
+++ src/main/java/com/mcdona22/pheonix/features/hello/presentation/HelloController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    public final HelloService helloService;
+    private final HelloService helloService;
 
     @GetMapping("/")
     public HelloResponse index() {
