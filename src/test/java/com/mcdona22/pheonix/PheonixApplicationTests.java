358f63ca36bca5bbfd57869ed695524bbdbc2227
John McDonald <mcdona22@gmail.com>
2025-11-25 11:25:02 +0000

    added files that had previously been skipped

---
+++ src/test/java/com/mcdona22/pheonix/PheonixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcdona22.pheonix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PheonixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/com/mcdona22/pheonix/features/hello/presentation/HelloControllerTest.java
@@ -0,0 +1,38 @@
+package com.mcdona22.pheonix.features.hello.presentation;
+
+import com.mcdona22.pheonix.features.hello.application.HelloService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+@Tag("unit")
+
+@ExtendWith(MockitoExtension.class)
+public class HelloControllerTest {
+    HelloController controller;
+    @Mock
+    private HelloService mockService;
+
+    @BeforeEach
+    void setUp() {
+        controller = new HelloController(mockService);
+    }
+
+    @Test
+    public void testServiceCalled() {
+        final var expected = new HelloResponse("message", "version");
+        when(mockService.getHelloResponse()).thenReturn(expected);
+
+        final var actual = controller.index();
+
+        assertEquals(actual, expected, "incorrect return value used");
+        verify(mockService, times(1)).getHelloResponse();
+
+    }
+}
