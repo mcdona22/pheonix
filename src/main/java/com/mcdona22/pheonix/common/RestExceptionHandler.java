2e0c315ed85f1a88d4b347da2c548f680724e0d7
John McDonald <mcdona22@gmail.com>
2025-11-27 16:30:02 +0000

    took out forcing the exception - that can be handled in test.  A bit scrappy still but lets work with it for now

--- src/main/java/com/mcdona22/pheonix/common/RestExceptionHandler.java
+++ src/main/java/com/mcdona22/pheonix/common/RestExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.mcdona22.pheonix.common;
 
-import jakarta.persistence.EntityNotFoundException;
+import jakarta.persistence.PersistenceException;
 import lombok.NonNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
@@ -14,12 +16,33 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+    Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @ExceptionHandler(EntityNotFoundException.class)
-    protected ResponseEntity<@NonNull ApiError> handleEntityNotFound(EntityNotFoundException ex,
+
+    @ExceptionHandler(PersistenceException.class)
+    protected ResponseEntity<@NonNull ApiError> handleEntityNotFound(PersistenceException ex,
                                                                      WebRequest request) {
+        logger.info("We entered this handler");
         final var error =
-                new ApiError(HttpStatus.BAD_REQUEST, ex.getMessage(), request.getContextPath().);
+                new ApiError(HttpStatus.BAD_REQUEST, ex.getMessage(), "");
         return ResponseEntity.badRequest().body(error);
     }
+
+    @ExceptionHandler(RuntimeException.class)
+    protected ResponseEntity<@NonNull ApiError> handleRuntimeException(RuntimeException ex,
+                                                                       WebRequest request) {
+        logger.info("Runtime Exception encountered: {}", ex.getMessage());
+        final var error =
+                new ApiError(HttpStatus.BAD_REQUEST, "Unspecified Error", ex.getMessage());
+        return ResponseEntity.badRequest().body(error);
+    }
+
+    @ExceptionHandler(PheonixRuntimeException.class)
+    protected ResponseEntity<@NonNull ApiError> handleRuntimeException(PheonixRuntimeException ex,
+                                                                       WebRequest request) {
+        final var error = ex.getError();
+        logger.info("Pheonix Runtime Exception encountered: {}", error.getMessage());
+        return ResponseEntity.badRequest().body(error);
+    }
+
 }
