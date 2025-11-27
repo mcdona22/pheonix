b4e80c1eee6d430fb3968aa50efaf12d791f7b2f
John McDonald <mcdona22@gmail.com>
2025-11-27 11:51:26 +0000

    used the correct lombok annotation

--- src/main/java/com/mcdona22/pheonix/common/ApiError.java
+++ src/main/java/com/mcdona22/pheonix/common/ApiError.java
@@ -1,16 +1,19 @@
 package com.mcdona22.pheonix.common;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
 
-@Data
 @AllArgsConstructor
 public class ApiError {
+    @Getter
     final private HttpStatus status;
+    @Getter
     final private LocalDateTime timestamp = LocalDateTime.now();
+    @Getter
     private String message;
+    @Getter
     private String debugMessage;
 }
